87ecc9760fc93ca8e14aa202ba63ed03f1c1fb74
JMGillum <JMGillum@protonmail.com>
2025-07-16 13:07:10 -0700

    Initial commit

---
+++ tree/formatString.py
@@ -0,0 +1,171 @@
+def removeNonAscii(text):
+    """
+    Keeps only the first 256 characters of extended ASCII. Probably bad for portability
+    """
+    newString = ""
+    for char in text:
+        if ord(char) <= 255:
+            newString = newString + char
+    return newString
+
+
+def placeString(string, length, start=0):
+    """
+    Places string at location start, and fills with spaces (at the end) until it is size length
+    """
+    if len(string) >= length:
+        return string
+    s = spacesString(length)
+    s = s[:start] + string + s[start + len(string) :]
+    return s
+
+
+def combineStrings(stringA, stringB, length, startA, startB):
+    if startB < startA:
+        return combineStrings(stringB, stringA, length, startB, startA)
+    stringA = placeString(stringA, length, startA)
+    string = stringA[:startB] + stringB + stringA[startB + len(stringB) :]
+    return string
+
+
+def spacesString(spaces):
+    """
+    Returns a string of spaces, with length equal to spaces parameter
+    """
+    return "".zfill(spaces).replace("0", " ")
+
+
+def tabulate(string, terminalWidth=80, spaces=8):
+    """
+    Given a string, splits the string across enough lines, such that there
+    each line begins with spaces number of spaces, and the total length of the line
+    does not exceed terminalWidth. Each line is terminated with '\n'
+    """
+    # Removes tabs from the original string
+    string = string.replace("\t", "")
+
+    # Splits the string into a list, separating at newlines already present (ends of paragraphs)
+    stringList = string.splitlines()
+
+    offset = terminalWidth - spaces  # the number of non-space characters for the line
+    tempstr = ""  # Stores the working string while it is being built up
+    checkstr = ""  # Used for checking if adding the next word will push the string over the length limit
+    tabulatedList = []  # A list comprised of each finished line
+    for item in stringList:
+        item = (
+            item.split()
+        )  # Splits the paragraphs up by words. Separating at every space
+        for word in item:  # Loops through every word
+            # If the word is longer than the amount of space for a single line
+            if len(word) > offset:
+                # Finishes the work in progress line
+                tabulatedList.append(f"{spacesString(spaces)}{checkstr}\n")
+                tempstr = ""
+                checkstr = ""
+
+                # Used to split the long word (typically links) into multiple lines
+                workingWord = word
+
+                # Splits word into offset sized lines
+                while len(workingWord) > offset:
+                    tabulatedList.append(
+                        f"{spacesString(spaces)}{workingWord[:offset]}\n"
+                    )
+                    workingWord = workingWord[offset:]
+
+                # Gives the end of the word (the part less than offset length) its own line
+                tabulatedList.append(f"{spacesString(spaces)}{workingWord}\n")
+                continue
+
+            # For normal words
+            checkstr = checkstr + word
+            if (
+                len(checkstr) < offset
+            ):  # simply adds new word to tempstr if it won't make it too long
+                tempstr = checkstr
+                tempstr = tempstr + " "
+                checkstr = tempstr
+            elif (
+                len(checkstr) == offset
+            ):  # Adds word, then pushes line to list and starts new line
+                tabulatedList.append(f"{spacesString(spaces)}{checkstr}\n")
+                tempstr = ""
+                checkstr = ""
+            else:  # Pushes current line to list, then starts a new line with word at the start
+                tabulatedList.append(f"{spacesString(spaces)}{tempstr}\n")
+                tempstr = word + " "
+                checkstr = tempstr
+
+        # Adds leftover words at the end of paragraph
+        tabulatedList.append(f"{spacesString(spaces)}{tempstr}\n")
+        tempstr = ""
+        checkstr = ""
+
+    newStr = ""
+    return newStr.join(tabulatedList)  # Combines list into a single string
+
+
+def enbox(
+    stringList,
+    terminalWidth,
+    leftPadding=1,
+    rightPadding=1,
+    leftMargin=0,
+    rightMargin=0,
+    fancy=False,
+):
+    """
+    Draws boxes around content. stringList is a list of strings of content. All entries in the list will be combined
+    into the same box. Using "%separator%" as an entry in the list will draw a horizontal line. terminalWidth is the width of the box.
+    Padding is the number of spaces between side walls and content.
+    Margin is the number of spaces between the edge of terminal and the side walls.
+    fancy is whether fancy characters will be used or basic characters.
+    """
+    boxWidth = terminalWidth - (leftMargin + rightMargin)
+    textBoxWidth = boxWidth - (leftPadding + rightPadding)
+
+    topLeft = "+"
+    topRight = "+"
+    bottomLeft = "+"
+    bottomRight = "+"
+    sideLeft = "+"
+    sideRight = "+"
+    vertical = "|"
+    horizontal = "-"
+
+    if fancy:
+        topLeft = "┌"
+        topRight = "┐"
+        bottomLeft = "└"
+        bottomRight = "┘"
+        sideLeft = "├"
+        sideRight = "┤"
+        vertical = "│"
+        horizontal = "─"
+
+    s = []
+
+    # Creates the top border of the box
+    s.append(f"{topLeft}{''.zfill(boxWidth - 2).replace('0', horizontal)}{topRight}")
+
+    for item in stringList:  # Loops through each item in the content list.
+        if item is not None:
+            # Creates a separating line in the box
+            if item == "%separator%":
+                # Adds a horizontal line
+                s.append(
+                    f"{sideLeft}{''.zfill(boxWidth - 2).replace('0', horizontal)}{sideRight}"
+                )
+            else:
+                # Breaks the content into lines that will fit in the text box.
+                listStrings = tabulate(item, textBoxWidth - 1, leftPadding).splitlines()
+                for line in listStrings:
+                    # Adds the side walls and appends the line to the list of lines
+                    line = placeString(line, textBoxWidth, 0)
+                    s.append(f"{vertical}{line}{vertical}")
+
+    # Creates bottom line
+    s.append(
+        f"{bottomLeft}{''.zfill(boxWidth - 2).replace('0', horizontal)}{bottomRight}"
+    )
+    return s
---
+++ tree/tree.py
@@ -0,0 +1,214 @@
+"""
+   Author: Josh Gillum              .
+   Date: 16 July 2025              ":"         __ __ 
+                                  __|___       \ V /
+                                .'      '.      | |
+                                |  O       \____/  |
+^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~
+
+    This is a general use tree class. It can handle arbitrary depths,
+    and can hold nodes of either strings or additional tree objects.
+    The tree works by building the tree from the deepest point up,
+    allowing it to figure out the size needed for shallower elements.
+
+    Also supports setting the display characters to either fancy or basic.
+    Basic display uses characters only found within 7 bit ASCII, while
+    fance uses other characters. If the tree's branches aren't displaying
+    correctly or are weird characters, try setting the fancy value to False.
+
+    Updates to the tree object will only affect this level. To update deeper
+    levels as well, the cascading_update line of functions must be used.
+    These will recursively update the deeper nodes, then the current nodes.
+
+
+^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~
+"""
+
+__version_info__ = ("1","0","0")
+__version__ = ".".join(__version_info__)
+
+
+from formatString import tabulate
+
+
+class Tree:
+    def __init__(
+        self,
+        name: str = None,
+        nodes: list | None = None,
+        fancy=False,
+        wrap=None,
+        width=None,
+    ):
+        self.set_name(name)
+        self.set_nodes(nodes)
+        self.set_fancy(fancy)
+        self.dirty = True
+        if width is None:
+            width = -1
+        if wrap is None:
+            wrap = -1
+        self.set_term_size(width)
+        self.set_line_wrap(wrap)
+
+    def set_name(self, name: str):
+        self.name = name
+        self.dirty = True
+
+    def set_nodes(self, nodes: list | None):
+        self.nodes = nodes
+        self.dirty = True
+
+    def cascading_update(self, set_fancy=None, line_width=None, term_width=None):
+        if set_fancy is not None:
+            self.cascading_set_fancy(set_fancy)
+        if line_width is not None:
+            self.cascading_set_line_wrap(line_width)
+        if term_width is not None:
+            self.cascading_set_term_size(term_width)
+
+    def cascading_set_fancy(self, set_fancy: bool):
+        if self.nodes is not None:
+            for item in self.nodes:
+                if isinstance(item, Tree):
+                    item.cascading_set_fancy(set_fancy)
+        self.set_fancy(set_fancy)
+
+    def cascading_set_term_size(self, width: int):
+        if self.nodes is not None:
+            for item in self.nodes:
+                if isinstance(item, Tree):
+                    item.cascading_set_term_size(width)
+        self.set_term_size(width)
+
+    def cascading_set_line_wrap(self, line_width: int):
+        if self.nodes is not None:
+            for item in self.nodes:
+                if isinstance(item, Tree):
+                    item.cascading_set_line_wrap(line_width)
+        self.set_line_wrap(line_width)
+
+    def set_fancy(self, set_fancy: bool):
+        self.fancy = set_fancy
+        self.set_characters()
+
+    def set_characters(self):
+        if self.fancy:
+            self.pipe = "│"
+            self.branch = "├─"
+            self.end = "└─"
+            self.space = " "
+        else:
+            self.pipe = "|"
+            self.branch = "|->"
+            self.end = "|->"
+            self.space = "  "
+        self.split_line = "~"
+
+    def set_term_size(self, width=80):
+        if width is not None:
+            self.width = width
+
+    def set_line_wrap(self, line_width: int):
+        if line_width is not None:
+            self.line_wrap = line_width
+
+    def print(self, as_a_string=False):
+        # if self.dirty: # Doesn't regenerate tree if no changes have been made.
+        # Sets entire tree as the single child node of tree. Necessary for proper spacing
+        # Saves values so they can be restored after building tree
+        name = self.name
+        nodes = self.nodes
+        child = Tree(
+            name, nodes, fancy=self.fancy, wrap=self.line_wrap, width=self.width
+        )
+        self.name = None
+        self.nodes = [child]
+        # Gets the tree, as a list of lines
+        self.list = self.recursive_generation(True)
+        # Converts the list to a single string
+        self.string = ""
+        for line in self.list:
+            self.string += f"{line}\n"
+        # Restores the original values of the name and nodes.
+        self.name = name
+        self.nodes = nodes
+        self.dirty = False
+        return self.string if as_a_string else self.list
+
+    def recursive_generation(self, last=False, prior_prefix=0):
+        """Generates a tree, recursively
+
+        Args:
+            last (bool, optional): _description_. Defaults to False.
+
+        Returns:
+            _type_: _description_
+        """
+        string = []
+        if self.name:
+            prefix = self.end if last else self.branch
+            try:
+                if self.line_wrap > 0: # If their is a max text width set, split into multiple lines
+                    # Splits the text into a number of lines with max lengths of self.line_wrap
+                    temp = tabulate(self.name, self.line_wrap, 0)
+                    temp = temp.split("\n") # Converts string of lines into a list of lines
+                    for j in range(len(temp)): # Loops through each line in the text
+                        if temp[j].strip() != "": # Ensures that this is not a blank line
+                            if j == 0:
+                                string.append(f"{prefix}{temp[j]}") # Adds branch character
+                            else: # Or character indicating that this is continuing from the previous line.
+                                string.append(f"{self.split_line}{temp[j]}") 
+                else: # No line-wrapping, so just add branch character and text
+                    string.append(f"{prefix}{self.name}")
+            except AttributeError:
+                string.append(f"{prefix}{self.name}")
+        if self.nodes is not None: # This object has some children nodes
+            for i in range(len(self.nodes)): # Loops through each node
+                item = self.nodes[i]
+                if isinstance(item, str):
+                    # If the node is just text, decide if this is the last child of the tree
+                    # Change the branch character if it is, otherwise use standard branch character.
+                    prefix = self.end if (i == len(self.nodes) - 1) else self.branch
+                    try:
+                        wrap = None
+                        if self.line_wrap is not None and self.line_wrap > 0:
+                            wrap = self.line_wrap
+                        if self.width is not None and self.width > 0:
+                            wrap = self.width - len(prefix) - prior_prefix
+                        if wrap is not None:
+                            temp = tabulate(item, wrap, 0)
+                            temp = temp.split("\n")
+                            for j in range(len(temp)):
+                                if temp[j].strip() != "":
+                                    if j == 0:
+                                        string.append(f"{prefix}{temp[j]}")
+                                    else:
+                                        if i == len(self.nodes) - 1:
+                                            wrap_prefix = self.space
+                                        else:
+                                            wrap_prefix = self.pipe
+                                        string.append(
+                                            f"{wrap_prefix}{self.split_line}{temp[j]}"
+                                        )
+                        else:
+                            string.append(f"{prefix}{item}")
+                    except AttributeError:
+                        string.append(f"{prefix}{item}")
+                elif isinstance(item, Tree):
+                    child_last = i == len(self.nodes) - 1
+                    prefix = self.end if last else self.branch
+                    child = item.recursive_generation(
+                        child_last, len(prefix) + prior_prefix
+                    )
+                    for j in range(1, len(child)):
+                        line = child[j]
+                        if i != len(self.nodes) - 1:
+                            child[j] = self.pipe
+                        else:
+                            child[j] = self.space
+                        if len(line) > 0 and line[0] != self.split_line:
+                            child[j] += self.space
+                        child[j] += line
+                    string += child
+        return string
