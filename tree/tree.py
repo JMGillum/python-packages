8ceb62e66d863f1b8fd54ded2d2ed68648d55e74
JMGillum <JMGillum@protonmail.com>
2025-07-17 16:01:16 -0700

    Moved version number to __init__.py

--- tree/tree.py
+++ tree/tree.py
@@ -24,8 +24,6 @@
 ^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~
 """
 
-__version_info__ = ("1","0","1")
-__version__ = ".".join(__version_info__)
 
 
 from formatString import tabulate
